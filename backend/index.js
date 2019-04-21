c52e657bde7a62dbec52e02542e388cf159fc97c
Max Taylor <max@cakerstream.com>
2019-04-21 20:46:09 +0100

    Cleaned up code

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`API server started on ${port}`));
